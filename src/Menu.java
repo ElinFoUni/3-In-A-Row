298b94fef905a91fabe69ab44112a9f9dff5c833
ElinFoUni <3ggapp24.tvinel@folkuniversitetet.nu>
2024-12-29 19:50:11 +0100

    minor menu stuff

--- src/Menu.java
+++ src/Menu.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 
+
 public class Menu {
+
+
+    public static void mainMenu() {
+        boolean end = false;
+        while (!end); {
+            System.out.println("Welcome to the menu");
+
+        }
+
+    }
+
+
 
 
 
